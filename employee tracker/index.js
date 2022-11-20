e078dbcea3458dd5a111c9c633a10dda17bfa7ab
druharo <111248621+druharo@users.noreply.github.com>
2022-11-20 15:12:59 +0300

    Add & update fns department, roles, employee.

--- employee tracker/index.js
+++ employee tracker/index.js
@@ -4,7 +4,7 @@ const cTable = require('console.table');
 const inquirer = require('inquirer');
 
 (async () => {
-    await showBanner("Employee\nManager", 'All your MySQL are Belong to US');
+    await showBanner("Employee\nManager", '-------------------------------');
     init();
 })();
 
@@ -14,42 +14,48 @@ const connection = mysql.createConnection({
     database: 'employee_tracker'
 });
 
+const prompts = [
+    {
+        name: 'View All Departments',
+        value: 'departments'
+    },
+    {
+        name: 'View All roles',
+        value: 'roles'
+    },
+    {
+        name: 'View All Employees',
+        value: 'employees'
+    },
+    {
+        name: 'Add Department',
+        value: 'addDepartment'
+    },
+    {
+        name: 'Add Role',
+        value: 'addRole'
+    },
+    {
+        name: 'Add Employee',
+        value: 'addEmployee'
+    },
+    {
+        name: 'Update Employee',
+        value: 'updateEmployee'
+    },
+    {
+        name: 'Quit',
+        value: 'quit'
+    }
+];
+
 function init() {
     inquirer
         .prompt({
             type: 'list',
             name: 'todo',
             message: 'What would you like to do?',
-            choices: [
-                {
-                    name: 'View All Departments',
-                    value: 'departments'
-                },
-                {
-                    name: 'View All roles',
-                    value: 'roles'
-                },
-                {
-                    name: 'View All employees',
-                    value: 'employees'
-                },
-                {
-                    name: 'Add Department',
-                    value: 'addDepartment'
-                },
-                {
-                    name: 'Add Role',
-                    value: 'addRole'
-                },
-                {
-                    name: 'Add Employee',
-                    value: 'addEmployee'
-                },
-                {
-                    name: 'Quit',
-                    value: 'quit'
-                }
-            ]
+            choices: prompts
         })
         .then((answers) => {
             if (answers.todo === "departments") {
@@ -57,13 +63,15 @@ function init() {
             } else if (answers.todo === "roles") {
                 show_roles();
             } else if (answers.todo === "employees") {
-                show_employees();  
+                show_employees();
             } else if (answers.todo === "addDepartment") {
                 add_department();
             } else if (answers.todo === "addRole") {
                 add_role();
             } else if (answers.todo === "addEmployee") {
                 add_employee();
+            } else if (answers.todo === "updateEmployee") {
+                update_employee();
             } else if (answers.todo === "quit") {
                 process.exit();
             }
@@ -74,12 +82,10 @@ function init() {
                 console.log(error);
             } else {
                 console.log("Something else went wrong");
-                console.log(error)
             }
         });
 }
-
-function show_departments(createConnection) {
+function show_departments() {
     connection.query(
         'SELECT * FROM `departments` ORDER BY name',
         function (err, results, fields) {
@@ -89,7 +95,7 @@ function show_departments(createConnection) {
         }
     )
 }
-function show_roles(createConnection) {
+function show_roles() {
     connection.query(
         'SELECT * FROM `roles` ORDER BY title',
         function (err, results, fields) {
@@ -99,21 +105,23 @@ function show_roles(createConnection) {
         }
     )
 }
-function show_employees(createConnection) {
+function show_employees() {
     connection.query(
-        `SELECT 
-    employees.id, 
-    employees.first_name, 
-    employees.last_name, 
-    roles.title, 
-    departments.name AS department,
-    roles.salary,
-    CONCAT (M.first_name, ' ', M.last_name) AS manager
-FROM employees 
-    INNER JOIN roles ON employees.role_id = roles.id
-    INNER JOIN departments ON roles.department_id = departments.id
-    LEFT OUTER JOIN employees M ON employees.manager_id = M.id
-    ;
+        `
+            SELECT
+                employees.id,
+                employees.first_name,
+                employees.last_name,
+                roles.title,
+                departments.name AS department,
+                roles.salary,
+                CONCAT(M.first_name, ' ', M.last_name) AS manager
+            FROM employees
+                INNER JOIN roles ON employees.role_id = roles.id
+                INNER JOIN departments ON roles.department_id = departments.id
+                LEFT OUTER JOIN employees M ON employees.manager_id = M.id
+            ;
+
         `,
         function (err, results, fields) {
             console.table(results); // results contains rows returned by server
@@ -122,13 +130,13 @@ FROM employees
         }
     )
 }
-function add_department(createConnection) {
+function add_department() {
     inquirer
         .prompt([
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is the name of the new department?',
+                message: 'What is the name of the new department',
             }
         ]).then((answers) => {
             connection.query(
@@ -199,15 +207,14 @@ function add_employee() {
         { name: 'Software Engineer', value: 3 },
         { name: 'Account Manager', value: 4 },
         { name: 'Accountant', value: 5},
-        { name: 'Legal Team Lead', value: 6}
+        { name: 'Legal Team Lead', value: 6},
         { name: 'Lawyer', value: 7}
     
     ];
     const managers = [
-        { name: 'Sales', value: 1 },
-        { name: 'Legal', value: 2 },
-        { name: 'Finance', value: 3 },
-        { name: 'Engineering', value: 4 }
+        { name: 'Ashley Rodriguez', value: 1 },
+        { name: 'Kunal Singh', value: 2 },
+        { name: 'Sarah Lourd', value: 3 },
     ];
     inquirer
         .prompt([
@@ -223,7 +230,7 @@ function add_employee() {
             },
             {
                 type: 'list',
-                name: 'roles_id',
+                name: 'role_id',
                 message: 'What is the employees role?',
                 choices: roles,
             },
@@ -235,13 +242,54 @@ function add_employee() {
             }
         ]).then((answers) => {
             connection.query(
-                `INSERT INTO employees (first_name, last_name, roles_id, manager_id) VALUE ('${answers.first_name}','${answers.last_name}' '${answers.roles_id}', '${answers.manager_id}')`,
+                `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUE ('${answers.first_name}','${answers.last_name}', ${answers.role_id}, ${answers.manager_id})`,
                 function (err, results, fields) {
                     if (err) {
                         console.log(err);
                     }
                     if (results) {
-                        console.log(`Added ${answers.title} to the roles`);
+                        console.log(`Added ${answers.first_name} to the Employees`);
+                    }
+                    process.exit();
+                }
+            );
+        });
+}
+function update_employeeRole() {
+    // To do get roles from the database via query
+    const roles = [
+        { name: 'Sale Person', value: 1 },
+        { name: 'Lead Engineer', value: 2 },
+        { name: 'Software Engineer', value: 3 },
+        { name: 'Account Manager', value: 4 },
+        { name: 'Accountant', value: 5},
+        { name: 'Legal Team Lead', value: 6},
+        { name: 'Lawyer', value: 7}
+    
+    ];
+    inquirer
+        .prompt([
+            {
+                type: 'number',
+                name: 'id',
+                message: 'What is the id of the imployee?',
+            },
+            {
+                type: 'list',
+                name: 'role_id',
+                message: 'What is the employees new role?',
+                choices: roles,
+            },
+            
+        ]).then((answers) => {
+            connection.query(
+                `INSERT INTO employees (id, role_id) VALUE (${answers.id},'${answers.role_id}')`,
+                function (err, results, fields) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    if (results) {
+                        console.log(`Added ${answers.id} to the Employees`);
                     }
                     process.exit();
                 }
--- employee tracker/schema.sql
+++ employee tracker/schema.sql
@@ -1,5 +1,6 @@
 -- mysql -u root employee_tracker < schema.sql
--- drop existing tables and recreate them
+-- dropped existing tables and recreate them
+
 
 DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS roles;
@@ -7,23 +8,23 @@ DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name varchar(30) UNIQUE
-);
+    name varchar(30)
+) ENGINE = InnoDB;
 
 CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title varchar(30) UNIQUE, 
     salary decimal,
     department_id integer REFERENCES departments(id)
-);
+) ENGINE = InnoDB;
 
 CREATE TABLE employees (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name varchar(30),
     last_name varchar(30),
     role_id integer REFERENCES roles(id), 
-    manager_id integer REFERENCES employees (id)
-);
+    manager_id integer REFERENCES employees(id)
+) ENGINE = InnoDB;
 
 INSERT INTO departments (name) VALUES ('Sales');
 INSERT INTO departments (name) VALUES ('Legal');
@@ -39,27 +40,26 @@ INSERT INTO roles (title,salary,department_id) VALUES ('Accountant', 125000,3);
 INSERT INTO roles (title,salary,department_id) VALUES ('Legal Team Lead', 250000,2);
 INSERT INTO roles (title,salary,department_id) VALUES ('Lawyer', 190000,2);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('John', 'Doe', 1, null);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Mike', 'Chan', 2, 1);  
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Ashley', 'Rodriguez', 3, null);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Kevin', 'Tupik', 4, 3);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Kunal', 'Singh', 5, null);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Malia', 'Brown', 6, 5);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Sarah', 'Lourd', 7, null);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Tom', 'Allen', 8, 7);
+INSERT INTO employees (first_name,last_name,role_id, manager_id) VALUES ('John', 'Doe', 1, null);
+INSERT INTO employees (first_name,last_name,role_id, manager_id) VALUES ('Mike', 'Chan', 2, 1);
+INSERT INTO employees (first_name,last_name,role_id, manager_id) VALUES ('Ashely', 'Rodrigez', 3, null);
+INSERT INTO employees (first_name,last_name,role_id, manager_id) VALUES ('Kevin', 'Tupik', 4, 3);
+INSERT INTO employees (first_name,last_name,role_id, manager_id) VALUES ('Kunal', 'Singh', 5, null);
+INSERT INTO employees (first_name,last_name,role_id, manager_id) VALUES ('Malia', 'Brown', 6, 5);
+INSERT INTO employees (first_name,last_name,role_id, manager_id) VALUES ('Sarah', 'Lourd', 7, null);
+INSERT INTO employees (first_name,last_name,role_id, manager_id) VALUES ('Tom', 'Allen', 8, 7);
 
-SELECT 
-    employees.id, 
-    employees.first_name, 
-    employees.last_name, 
-    roles.title, 
+-- https://blog.devart.com/mysql-joins-tutorial-with-examples.html
+SELECT
+    employees.id,
+    employees.first_name,
+    employees.last_name,
+    roles.title,
     departments.name AS department,
     roles.salary,
-    CONCAT (M.first_name, ' ', M.last_name) AS manager
-FROM employees 
+    CONCAT(M.first_name, ' ', M.last_name) AS manager
+FROM employees
     INNER JOIN roles ON employees.role_id = roles.id
     INNER JOIN departments ON roles.department_id = departments.id
     LEFT OUTER JOIN employees M ON employees.manager_id = M.id
-    ;
- 
-
+;
