9918377d6ca94baf4e6edb16522bdea5a81ffc16
druharo <111248621+druharo@users.noreply.github.com>
2022-11-20 11:14:05 +0300

    changes in schema, view all employees

--- employee tracker/index.js
+++ employee tracker/index.js
@@ -30,6 +30,10 @@ function init() {
                     value: 'roles'
                 },
                 {
+                    name: 'View All employees',
+                    value: 'employees'
+                },
+                {
                     name: 'Quit',
                     value: 'quit'
                 }
@@ -40,6 +44,8 @@ function init() {
                 show_departments();
             } else if (answers.todo === "roles") {
                 show_roles();
+            } else if (answers.todo === "employees") {
+                show_employees();  
             } else if (answers.todo === "quit") {
                 process.exit();
             }
@@ -67,6 +73,29 @@ function show_departments(createConnection) {
 function show_roles(createConnection) {
     connection.query(
         'SELECT * FROM `roles` ORDER BY title',
+        function (err, results, fields) {
+            console.table(results); // results contains rows returned by server
+            // console.log(fields); // fields contains extra meta data about results, if available
+            process.exit();
+        }
+    )
+}
+function show_employees(createConnection) {
+    connection.query(
+        `SELECT 
+    employees.id, 
+    employees.first_name, 
+    employees.last_name, 
+    roles.title, 
+    departments.name AS department,
+    roles.salary,
+    CONCAT (M.first_name, ' ', M.last_name) AS manager
+FROM employees 
+    INNER JOIN roles ON employees.role_id = roles.id
+    INNER JOIN departments ON roles.department_id = departments.id
+    LEFT OUTER JOIN employees M ON employees.manager_id = M.id
+    ;
+        `,
         function (err, results, fields) {
             console.table(results); // results contains rows returned by server
             // console.log(fields); // fields contains extra meta data about results, if available
--- employee tracker/schema.sql
+++ employee tracker/schema.sql
@@ -1,16 +1,65 @@
-CREATE TABLE departments (id serial, name text);
-CREATE TABLE roles (id serial, title text, salary INT, department text);
+-- mysql -u root employee_tracker < schema.sql
+-- drop existing tables and recreate them
+
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(30)
+);
+
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title varchar(30), 
+    salary decimal,
+    department_id integer REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name varchar(30),
+    last_name varchar(30),
+    role_id integer REFERENCES roles(id), 
+    manager_id integer REFERENCES employees (id)
+);
 
 INSERT INTO departments (name) VALUES ('Sales');
 INSERT INTO departments (name) VALUES ('Legal');
 INSERT INTO departments (name) VALUES ('Finance');
 INSERT INTO departments (name) VALUES ('Engineering');
 
-INSERT INTO roles (title,salary,department) VALUES ('Sales Lead', 100000,'Sales');
-INSERT INTO roles (title,salary,department) VALUES ('Salesperson', 80000,'Sales');
-INSERT INTO roles (title,salary,department) VALUES ('Lead Engineer', 150000,'Engineering');
-INSERT INTO roles (title,salary,department) VALUES ('Software Engineer', 120000,'Engineering');
-INSERT INTO roles (title,salary,department) VALUES ('Account Manager', 160000,'Finance');
-INSERT INTO roles (title,salary,department) VALUES ('Accountant', 125000,'Finance');
-INSERT INTO roles (title,salary,department) VALUES ('Legal Team Lead', 250000,'Legal');
-INSERT INTO roles (title,salary,department) VALUES ('Lawyer', 190000,'Legal');
+INSERT INTO roles (title,salary,department_id) VALUES ('Sales Lead', 100000, 1);
+INSERT INTO roles (title,salary,department_id) VALUES ('Salesperson', 80000, 1);
+INSERT INTO roles (title,salary,department_id) VALUES ('Lead Engineer', 150000, 4);
+INSERT INTO roles (title,salary,department_id) VALUES ('Software Engineer', 120000, 4);
+INSERT INTO roles (title,salary,department_id) VALUES ('Account Manager', 160000,3);
+INSERT INTO roles (title,salary,department_id) VALUES ('Accountant', 125000,3);
+INSERT INTO roles (title,salary,department_id) VALUES ('Legal Team Lead', 250000,2);
+INSERT INTO roles (title,salary,department_id) VALUES ('Lawyer', 190000,2);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('John', 'Doe', 1, null);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Mike', 'Chan', 2, 1);  
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Ashley', 'Rodriguez', 3, null);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Kevin', 'Tupik', 4, 3);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Kunal', 'Singh', 5, null);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Malia', 'Brown', 6, 5);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Sarah', 'Lourd', 7, null);
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Tom', 'Allen', 8, 7);
+
+SELECT 
+    employees.id, 
+    employees.first_name, 
+    employees.last_name, 
+    roles.title, 
+    departments.name AS department,
+    roles.salary,
+    CONCAT (M.first_name, ' ', M.last_name) AS manager
+FROM employees 
+    INNER JOIN roles ON employees.role_id = roles.id
+    INNER JOIN departments ON roles.department_id = departments.id
+    LEFT OUTER JOIN employees M ON employees.manager_id = M.id
+    ;
+ 
+
